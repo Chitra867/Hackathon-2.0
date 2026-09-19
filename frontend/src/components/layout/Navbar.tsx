9645b0d2cf896fc89bb51bb507c5146ba6a50392
Sudehna Devkota <sudeshnadevkota@gmail.com>
2026-09-19 11:13:35 +0545

    logo image update

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -105,7 +105,7 @@ export const Navbar: React.FC = () => {
 
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
 
-        <div className="flex h-16 items-center justify-between">
+        <div className="flex h-16 items-center justify-between">  
 
 
           {/* =================================================
